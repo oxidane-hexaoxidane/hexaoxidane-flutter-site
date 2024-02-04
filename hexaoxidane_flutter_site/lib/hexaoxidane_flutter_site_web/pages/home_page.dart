f938084ac604692555162f9c6ce49e8125828290
Oxidane <oxidane@hexaoxidane.com>
2024-02-04 13:20:24 +0800

    ~ HexaOxidane.Flutter.Site: Update more appropriate fonts

--- hexaoxidane_flutter_site/lib/hexaoxidane_flutter_site_web/pages/home_page.dart
+++ hexaoxidane_flutter_site/lib/hexaoxidane_flutter_site_web/pages/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:hexaoxidane_flutter_site/hexaoxidane_flutter_engine/hexaoxidane_flutter_engine.dart';
 
 class HomePage extends StatefulWidget {
@@ -12,6 +13,21 @@ class HomePage extends StatefulWidget {
 class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
+    const Color backgroundColor = Color.fromARGB(220, 107, 65, 39);
+    const Color textColor = Color.fromARGB(255, 228, 205, 104);
+
+    TextStyle headerTextStyle = GoogleFonts.cinzel(
+      color: textColor,
+      fontWeight: FontWeight.bold,
+      fontSize: 30,
+    );
+
+    TextStyle displayTextStyle = GoogleFonts.literata(
+      color: textColor,
+      fontWeight: FontWeight.normal,
+      fontSize: 20,
+    );
+
     return PageScaffold(
       background: Container(
         alignment: Alignment.center,
@@ -43,19 +59,22 @@ class _HomePageState extends State<HomePage> {
           SizedBox(
             width: double.infinity,
             child: ColoredBox(
-              color: Colors.white,
+              color: backgroundColor,
               child: Padding(
                 padding: const EdgeInsets.all(20),
                 child: Column(
                   children: [
                     // const SizedBox(height: 20),
                     // const Text('We are making the game...', style: TextStyles.display),
-                    const Text(
+                    Text(
                       "DOCTOR! is an adult game set in the medieval era during the times of plague, and you're a doctor. There were many types of diseases looming around such as cold, fever, etc, but there is one particular type that baffles everyone. It's the kind of illness that mostly affects women, and no one knows when it will hit; one just kinda gets it, and what's scary is you don't know what's gonna happen to them as some became depressed, some got lonely, and others even got rabid. \n\nFortunately, as the doctor, you know the cure. You know exactly what these women need. A huge, big, and juicy cock only you can provide.",
-                      style: TextStyles.display,
+                      style: displayTextStyle,
                     ),
                     const SizedBox(height: 100),
-                    const Text('Stay tuned!', style: TextStyles.heading3),
+                    Text(
+                      'Stay tuned!',
+                      style: headerTextStyle,
+                    ),
                     SizedBox(
                       height: 50,
                       child: Row(
@@ -86,7 +105,10 @@ class _HomePageState extends State<HomePage> {
                       ),
                     ),
                     const SizedBox(height: 50),
-                    const Text('Join our maling list?', style: TextStyles.heading3),
+                    Text(
+                      'Join our maling list?',
+                      style: headerTextStyle,
+                    ),
                     const SizedBox(height: 5),
                     ElevatedButton(
                       onPressed: () {
